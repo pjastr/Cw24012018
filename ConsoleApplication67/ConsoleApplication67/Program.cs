0c4088003a24edc44b14e47d236c03a00d5451ec
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-24 18:58:56 +0100

    Add files via upload

---
+++ ConsoleApplication67/ConsoleApplication67/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<DateTime>> prac = new Dictionary<string, List<DateTime>>();
+            prac.Add("Nowak", new List<DateTime>() { new DateTime(2017,2,11), new DateTime(2017,2,13)});
+            prac.Add("Kowalski", new List<DateTime>() { new DateTime(2017, 2, 1) });
+            foreach(var element in prac)
+            {
+                string opis = "";
+                opis = "Pracownik " + element.Key + " jego dyżury" + Environment.NewLine;
+                foreach(var el2 in element.Value)
+                {
+                    opis += el2.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US")) + Environment.NewLine;
+                }
+                Console.WriteLine(opis);
+            }
+            Console.ReadKey();
+        }
+    }
+}
