e3fd34cb9068eac0ca540887f7d08ff687b610b0
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-24 18:43:16 +0100

    Add files via upload

---
+++ ConsoleApplication65/ConsoleApplication65/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class WlasnaKlasa
+    {
+        private static WlasnaKlasa instancja;
+        private WlasnaKlasa()
+        {
+
+        }
+
+        public static WlasnaKlasa Utworz()
+        {
+            if (instancja == null)
+            {
+                instancja = new WlasnaKlasa();
+            }
+            return instancja;
+        }
+    }
+}
---
+++ ConsoleApplication66/ConsoleApplication66/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firma f = new Firma();
+            f.DodajPracownika("Jan Kowalski");
+            f.DodajPracownika("Adam Nowak");
+            foreach (var prac in f)
+            {
+            Console.WriteLine(prac);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Firma:IEnumerable
+    {
+        List<string> pracownicy;
+        public Firma()
+        {
+            pracownicy = new List<string>();
+        }
+
+        public void DodajPracownika(string p1)
+        {
+            pracownicy.Add(p1);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach(var element in pracownicy)
+            {
+                yield return element;
+            }
+        }
+    }
+}
